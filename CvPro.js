3114bc506aa6a3041b702861b9a613d64a3f1a30
Asanchezsa04 <149249238+Asanchezsa04@users.noreply.github.com>
2023-10-28 20:50:10 +0200

    Delete CvPro.js

--- CvPro.js
+++
@@ -1,8 +0,0 @@
-function canvia_idioma(){
-	var idioma;
-	idioma = document.getElementById('Idiomes').value;
-	console.log(idioma)
-	if(idioma=="Català") location.href="https://github.com/Asanchezsa04/HMP1_CVCAT"
-	else if(idioma=="Español") location.href="https://github.com/Asanchezsa04/HMP1_CVES"
-	else if(idioma=="English") location.href="https://asanchezsa04.github.io/HMP1_CV/"
-}
